d4976bb791a1daaec436990d7f7ff7aca5e76364
KirilIliev24 <43208740+KirilIliev24@users.noreply.github.com>
2021-05-19 15:56:31 +0300

    first commit

---
+++ SEP6_TEST/SEP6_TEST/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEP6_TEST
+{
+    class Class1
+    {
+    }
+}
---
+++ SEP6_TEST/SEP6_TEST/Models/StartTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SEP6_TEST.Models
+{
+    public partial class StartTable
+    {
+        public string StartColum { get; set; }
+    }
+}
