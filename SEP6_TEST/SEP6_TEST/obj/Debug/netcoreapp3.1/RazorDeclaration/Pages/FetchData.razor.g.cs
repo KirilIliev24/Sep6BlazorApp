25ff49f939953c6470401bdcf299a053e3d127f8
HarryTomovski <43003754+HarryTomovski@users.noreply.github.com>
2021-05-23 18:25:33 +0200

    Revert "test"
    This reverts commit fe97c2e94ae80f0b38984f25193bb2acdd3cf894.

--- SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,91 +13,91 @@ namespace SEP6_TEST.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 1 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 2 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 3 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 4 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 5 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 6 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 7 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 8 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 9 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 3 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 4 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.ApiAccess;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 5 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.ApiModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 6 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.DbAccess;
 
 #line default
@@ -112,7 +112,7 @@ using SEP6_TEST.DbAccess;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 45 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 45 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
        
     private List<Movie> Movies = new List<Movie>();
 
