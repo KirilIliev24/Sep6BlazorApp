8dce9923e0134d743a58e9aa9a4392364933fed5
Mintiss <m.miklasevicius3@gmail.com>
2021-05-24 10:58:05 +0200

    Revert "cummint"
    This reverts commit fa9042cf96c9ee818824de2bd80798521efd306c.

--- SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,99 +13,92 @@ namespace SEP6_TEST.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 1 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 2 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 3 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 4 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 5 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 6 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 7 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 8 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 9 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 3 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 4 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.ApiAccess;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 5 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.ApiModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 6 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
 using SEP6_TEST.DbAccess;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 7 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
-using SEP6_TEST.Components;
 
 #line default
 #line hidden
@@ -119,7 +112,7 @@ using SEP6_TEST.Components;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 33 "D:\SEP6\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
+#line 45 "C:\Users\Harry\OneDrive\Documents\SEP06\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\Pages\FetchData.razor"
        
     private List<Movie> Movies = new List<Movie>();
 
@@ -130,15 +123,13 @@ using SEP6_TEST.Components;
     protected override async Task OnInitializedAsync()
     {
         Loading = true;
-        //await moviesDb.GetAllMovies();
+        await moviesDb.GetAllMovies();
         Movies = moviesDb.Movies;
 
         //should get the base info for every movie, add all async methods to a list and run them async
 
         Loading = false;
     }
-
-    Movie movie = new Movie { Id = 1, rating = new Rating { MovieId = 1, Rating1 = 6.6, Votes = 245}, Title = "Fat & Furious 6", Year = 2025 };
 
 #line default
 #line hidden
