d7eb23f332c6d407178e6982ef4d07ccdcc90cf9
KirilIliev24 <43208740+KirilIliev24@users.noreply.github.com>
2021-05-24 14:15:33 +0300

    commit something

--- SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
+++ SEP6_TEST/SEP6_TEST/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
@@ -13,63 +13,63 @@ namespace SEP6_TEST
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 1 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 2 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 3 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 4 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 5 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 6 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 7 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 8 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Harry\OneDrive\Documents\VIA\SEP06\SEP6BlazorAppMain\Sep6BlazorApp\SEP6_TEST\SEP6_TEST\_Imports.razor"
+#line 9 "D:\VIA\6th semester\SEP 6\Testing\SEP6_TEST\SEP6_TEST\_Imports.razor"
 using SEP6_TEST.Shared;
 
 #line default
